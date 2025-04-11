85b1e8235c24d6ff8e8ae3070bccd55a1e050cbb
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-04-11 14:17:05 -0300

    database status

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,22 @@
-import { Client } from 'pg'
+import {Client} from 'pg'
 
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    return await client.query(queryObject);
+  }catch(err) {
+    console.error(err);
+  }finally {
+    await client.end();
+  }
+
 }
 
 export default {
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -3,8 +3,10 @@ test("Get to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
