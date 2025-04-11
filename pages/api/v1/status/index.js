b2b6b666f09affb7eb55a57f5eeb7281097172c3
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-04-10 22:02:20 -0300

    update

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from '../../../../infra/database'
+
+async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1;')
+  console.log(result)
+  console.log("*******************")
   res.status(200).send({ message: "eu te amo" });
 }
 
