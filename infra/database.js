e99cb25ef4cda3094badd4fe8bb32884bf6c339c
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-04-13 05:02:17 -0300

    refactored all this file

--- infra/database.js
+++ infra/database.js
@@ -1,15 +1,9 @@
 import {Client} from 'pg'
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
+  let client;
   try {
+    client = await getNewClient();
     return await client.query(queryObject);
   }catch(err) {
     console.error(err);
@@ -19,6 +13,32 @@ async function query(queryObject) {
 
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+
 export default {
-  query: query,
+  query,
+  getNewClient,
+}
+
+
+function getSSLValues(){
+  if(process.env.POSTGRES_CA){
+    return {
+      ca: process.env.POSTGRES_CA,
+    }
+    return process.env.NODE_ENV === 'production' ? true : false;
+  }
 }
